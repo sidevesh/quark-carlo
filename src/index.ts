ebbd00ee3d9c4be8de8948c4d85ebe84f10c3cc8
SiDevesh <sid.swapnildevesh@@gmail.com>
2018-11-12 04:55:28 +0530

    Initial desktop file generation

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
 import {Command, flags} from '@oclif/command'
+import { platform as osPlatform } from 'os'
 import { format } from 'util'
-import { dir } from 'tmp'
-import { cp, echo, exec, pwd, cd } from 'shelljs' 
+import { dir as tempDir } from 'tmp'
+import { cp, echo, exec, pwd, cd, ls, cat } from 'shelljs' 
 const { exec:pkgExec } = require('pkg')
+
+const placeholderAppName = 'quark-carlo-placeholder'
+
+const platform = osPlatform()
+const isLinux = () => platform === 'linux'
+const isWIndows = () => platform === 'win32'
+const isMac = () => platform === 'darwin'
+
+const linuxInstallationDesktopFilesPath:string = '~/.local/share/applications'
+const linuxInstallationDesktopFilesIconFilesPath:string = '~/.local/share/icons/hicolor/128x128/apps'
 
 const filenameSafe = (str:string) => str.replace(/[^a-z0-9]/gi, '_').toLowerCase()
 
 class QuarkCarlo extends Command {
-  static description = 'describe the command here'
+  static description = 'Create native app from any web app, optionally install a shortcut so that the app shows up in the application menu'
 
   static flags = {
     version: flags.version({ char: 'v' }),
     help: flags.help({ char: 'h' }),
     name: flags.string({ char: 'n', description: 'name of application', required: true }),
     url: flags.string({ char: 'u', description: 'url to load in application', required: true }),
+    install: flags.boolean({ char: 'i', description: 'Install a shortcut so that the app shows up in the application menu', default: false }),
     dimensions: flags.string({
       char: 'd',
       description: 'Dimensions of application window as [width]x[height], for example 1280x720',
@@ -23,10 +35,13 @@ class QuarkCarlo extends Command {
 
   async run() {
     const { flags } = this.parse(QuarkCarlo)
-    const { name, url, dimensions } = flags
+    const { name, url, dimensions, install } = flags
     const execPath:string = pwd().valueOf()
+    const binaryName:string = `${filenameSafe(name)}-quark`
+
     let width:number = 1280
     let height:number = 720
+
     try {
       if (dimensions === undefined) throw 'dimensions undefined'
       if (dimensions.split('x').length !== 2) throw 'dimensions invalid format'
@@ -44,23 +59,33 @@ class QuarkCarlo extends Command {
       width,
       height,
     })
-    dir({ unsafeCleanup: true }, (err, path) => {
+    tempDir({ unsafeCleanup: true }, (err, tempDirPath) => {
       if (err) throw err
-      cp('-R', `${__dirname}/../app/*`, path)
+      cp('-R', `${__dirname}/../app/*`, tempDirPath)
       this.log(`Config options:`)
-      echo(config).to(`${path}/config.json`)
-      cd(path)
+      echo(config).to(`${tempDirPath}/config.json`)
+      cd(tempDirPath)
       this.log('Installing dependencies...')
       exec('npm install', { silent: true }, (code) => {
         cd(execPath)
         if (code === 0) {
           this.log('Installing dependencies succeeded...')
           this.log('Building binaries...')
-          pkgExec([ path, '--out-path', `${path}` ])
+          pkgExec([ tempDirPath, '--out-path', tempDirPath, '--targets', 'host' ])
             .then(() => {
-              cp(`${path}/quark-carlo-placeholder-linux`, `./${filenameSafe(name)}-linux`)
-              cp(`${path}/quark-carlo-placeholder-macos`, `./${filenameSafe(name)}-macos`)
-              cp(`${path}/quark-carlo-placeholder-win.exe`, `./${filenameSafe(name)}-win.exe`)
+              ls(tempDirPath)
+                .filter(fileName => fileName === placeholderAppName)
+                .forEach(fileName => cp(`${tempDirPath}/${fileName}`, `${execPath}/${binaryName}`))
+              if (install) {
+                this.log('Installing shortcut...')
+                if (isLinux()) {
+                  cat(`${__dirname}/../installation/linux/app.desktop`)
+                    .sed('@@NAME@@', name)
+                    .sed('@@PATH@@', `${execPath}/${binaryName}`)
+                    .sed('@@FILENAME@@', `${binaryName}`)
+                    .to(`${linuxInstallationDesktopFilesPath}/${binaryName}.desktop`)
+                }
+              }
             })
         } else {
           this.error('npm install failed...')
